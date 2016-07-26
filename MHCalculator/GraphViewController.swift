fb72cb868af776bcfedc8b668943ef58c3b12e5a
Minh Hoang TCU <minh.hoang@tcu.edu>
2016-07-26 14:52:01 -0500

    Added another MVC and segue

---
+++ MHCalculator/GraphViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.swift
+//  MHCalculator
+//
+//  Created by Minh Hoang on 7/26/16.
+//  Copyright © 2016 Minh Hoang. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+}
